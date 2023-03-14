831c91115f7c475288edd4a71f40f4f8e79700fe
Junsong Cang <junsong.cang@sns.it>
2023-03-14 22:09:03 +0800

    First order of business: code style adjusted with VS codes

--- HyRec/hyrectools.h
+++ HyRec/hyrectools.h
@@ -14,5 +14,4 @@ void free_3D_array(double ***matrix, unsigned n1, unsigned n2);
 void maketab(double xmin, double xmax, unsigned Nx, double *xtab);
 double rec_interp1d(double x0, double dx, double *ytab, unsigned int Nx, double x);
 double rec_interp2d(double x10, double dx1, double x20, double dx2, double **ytab,
-		    unsigned int Nx1, unsigned int Nx2, double x1, double x2);
-
+										unsigned int Nx1, unsigned int Nx2, double x1, double x2);

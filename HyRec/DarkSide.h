25f55bdc4dbeb05f9b28c490eebe2b03f9af2851
Junsong Cang <junsong.cang@sns.it>
2023-03-17 18:41:33 +0800

    Added forecast datasets for CVL_3000, CVL_4000 and PICO_BaseLine

--- HyRec/DarkSide.h
+++ HyRec/DarkSide.h
@@ -1,11 +1,19 @@
-/* Dark Matter Energy injection module */
+/* Dark Matter Energy injection module 
+main reference : 2108.13256
+*/
+
 
 // Rho_cr * c^2 /h^2, in eV/cm^3
 #define Rhocr_C2_no_h2 1.054041699E4
 #define Nearly_Zero 1.0E-50
 #define Zmax_Energy_Injection 2000
-#define debug_mode 0
 
+/* Set number of timesteps in integrating PBH deposition rate
+100 takes 8 seconds and should be good enough
+*/
+#define PBH_Integration_Size 100
+
+#define debug_mode 0
 
 void Validate_Inputs(REC_COSMOPARAMS *params)
 {
@@ -14,9 +22,11 @@ void Validate_Inputs(REC_COSMOPARAMS *params)
     2. PBH Spin
     3. PBH Model: Currently only support [1 2 3]
     */
-    int Particle_Channel, PBH_Model;
+    int Particle_Channel, PBH_Model, PBH_Distribution;
     Particle_Channel = Convert_to_Int(params->DM_Channel);
     PBH_Model = Convert_to_Int(params->PBH_Model);
+    PBH_Distribution = Convert_to_Int(params->PBH_Distribution);
+
     if ((Particle_Channel < 1) || (Particle_Channel > 12))
     {
         printf("Error from Validate_Inputs@HyRec: Unknown particle channel selected\n");
@@ -31,6 +41,19 @@ void Validate_Inputs(REC_COSMOPARAMS *params)
     {
         printf("Error from Validate_Inputs@HyRec: Wrong choice of PBH_Model\n");
         exit(1);
+    }
+    if (fabs(params->PBH_PWL_Gamma)<0.001)
+    {
+        printf("Error: PBH Power-Law index cannot be 0.\n");
+        exit(1);
+    }
+    if (PBH_Distribution == 3)
+    {
+        if ((params->Mbh) > (params->PBH_PWL_Mmax))
+        {
+            printf("Error: Mmin > Mmax in power-law.\n");
+            exit(1);
+        }
     }
 }
 
@@ -61,7 +84,6 @@ double dEdVdt_ann_inj(double z, REC_COSMOPARAMS *params)
     return r;
 }
 
-
 double dEdVdt_Hawking_inj(double z, double Mbh, REC_COSMOPARAMS *params)
 {
     // Hawking Radiation injection, Normalised to mbh>10^17 g
@@ -82,7 +104,6 @@ double dEdVdt_decay_dep(double z, REC_COSMOPARAMS *params, int dep_channel)
     return r;
 }
 
-
 double dEdVdt_Hawking_Mono_dep(double z, double Mbh, REC_COSMOPARAMS *params, int dep_channel)
 {
     // Hawking Radiation monochromatic deosition rate
@@ -93,11 +114,137 @@ double dEdVdt_Hawking_Mono_dep(double z, double Mbh, REC_COSMOPARAMS *params, in
     return r;
 }
 
+
+double PBH_Mass_Function(double m, REC_COSMOPARAMS *params)
+{
+  // return PBH mass function, result has dimension of m^-1, unit of m is determined by PBH_Model
+  double r, mc, sbh, mmin, mmax, y, pi, x;
+  int PBH_Distribution;
+  mc = params->Mbh;
+  sbh = params->PBH_Lognormal_Sigma;
+  mmin = mc;
+  mmax = params->PBH_PWL_Mmax;
+  y = params->PBH_PWL_Gamma;
+  pi = 3.141592653589793;
+  PBH_Distribution = Convert_to_Int(params->PBH_Distribution);
+
+  if (PBH_Distribution == 2)
+  {
+    // log-normal
+    r = pow(log(m / mc), 2);
+    r = -r / (2 * sbh * sbh);
+    r = exp(r) / (m * sbh * sqrt(2 * pi));
+  }
+  else if (PBH_Distribution == 3)
+  {
+    // Power-law
+    if ((mmin <= m) && (m <= mmax))
+    {
+      r = y * pow(m, y - 1);
+      r = r / (pow(mmax, y) - pow(mmin, y));
+    }
+    else
+    {
+      r = 0.0;
+    }
+  }
+  else if (PBH_Distribution == 4)
+  {
+    x = pow(m / mc, 2.85);
+    r = x * exp(-x);
+    r = 3.2 * r / mc;
+  }
+
+  r = 0;
+  return r;
+}
+
+double Integrate_PBH_dEdVdt(double z, REC_COSMOPARAMS *params, int dep_channel)
+{
+  // Find dEdVdt_dep for general PBH distributions, applicable to both Hawking Radiation and Accreting PBH
+  int PBH_Distribution, PBH_Model, idx;
+  double Mmin, Mmax, Tmin, Tmax, r, x1, x2, x1_, x2_, xc, sbh, dx, x, f1, f2, f, m;
+  PBH_Distribution = Convert_to_Int(params->PBH_Distribution);
+  PBH_Model = Convert_to_Int(params->PBH_Model);
+  sbh = params->PBH_Lognormal_Sigma;
+
+  if ((PBH_Model == 2) || (PBH_Model == 3))
+  {
+    Tmax = 0.9999 * Kerr_PBH_Temperature_Axis[Kerr_PBH_Temperature_Size - 1];
+    Tmin = 1.0001 * Kerr_PBH_Temperature_Axis[0];
+    Mmin = 1.06E22 / Tmax;
+    Mmax = 1.06E22 / Tmin;
+  }
+  else
+  {
+    printf("Error: Wrong choice of PBH_Model.\n");
+    exit(1);
+  }
+
+  // Doing integration in log space, x defined as log(m)
+  x1 = log(Mmin);
+  x2 = log(Mmax);
+  // Optimise integration boundary
+  if (PBH_Distribution == 2)
+  {
+    // log-normal: +- 5 sigma
+    xc = log(params->Mbh);
+    x1_ = xc - 5 * sbh;
+    x2_ = xc + 5 * sbh;
+  }
+  else if (PBH_Distribution == 3)
+  {
+    // power-law: [Mmin, Mmax]
+    x1_ = log(params->Mbh);
+    x2_ = log(params->PBH_PWL_Mmax);
+  }
+  // updating
+  x1 = fmax(x1, x1_);
+  x2 = fmin(x2, x2_);
+
+  dx = (x2 - x1) / ((double)PBH_Integration_Size - 1.0);
+  r = 0.0;
+  x = x1;
+
+  for (idx = 0; idx < PBH_Integration_Size; idx++)
+  {
+    m = exp(x);
+    if ((PBH_Model == 2) || (PBH_Model == 3))
+    {
+      f1 = dEdVdt_Hawking_Mono_dep(z, m, params, dep_channel);
+    }
+    else
+    {
+      printf("Accreting PBH not ready.\n");
+      exit(1);
+    }
+    f2 = PBH_Mass_Function(m, params);
+    f = f1 * f2 * m;
+    r = r + f * dx;
+    x = x + dx;
+  }
+
+  return r;
+}
+
 double dEdVdt_Hawking_dep(double z, REC_COSMOPARAMS *params, int dep_channel)
 {
     // Hawking Radiation for general mass distributions
-    // Currently only allow monochromatic, more on the way
-    return dEdVdt_Hawking_Mono_dep(z, params->Mbh, params, dep_channel);
+    int PBH_Distribution;
+    double r;
+
+    PBH_Distribution = Convert_to_Int(params->PBH_Distribution);
+    if (PBH_Distribution == 1)
+    {
+        r = dEdVdt_Hawking_Mono_dep(z, params->Mbh, params, dep_channel);
+    }
+    else
+    {
+        r = Integrate_PBH_dEdVdt(z, params, dep_channel);
+    }
+  
+    return r;
+
 }
 
 double dEdVdt_ann_dep(double z, REC_COSMOPARAMS *params, int dep_channel)
